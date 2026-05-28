a43323caf9c530d5c24e2a2f3692a24911076244
alexandreorcsilva <alexandreorcsilva@gmail.com>
2026-05-28 01:31:28 +0100

    1 sem quit

--- cenario1e3.py
+++ cenario1e3.py
@@ -1,26 +1,29 @@
-from Projectiles import *
+from engine import *
 from entities import *
 from cen2 import cen2
-from trajetoria import recording_window, record
-from gravar_trajectory import *
+from gravar_trajetoria import *
 import time
 
 
 def cen1():
-    # define um valor placeholder para as variáveis
+
 
     angle, vel0, h0, interval = 20, 40, 48, 0.1
-    v_window = InputDialog(angle, vel0)
-    option = v_window.interaction()
-    v_window.close()
+
     win = GraphWin("Basketball Game", 400, 400, autoflush=False)
     win.setCoords(-10, -10, 210, 155)
+
+    Line(Point(205, -10), Point(205, 155)).draw(win)
+
     # cria a linha de base
     marked_distance_line = Static(center=Point(100, 0), shape="metric", color="black", outline="black", window=win)
-
     # cria o cesto
     basket = Static(center=Point(160, 100), shape="basket", radius=3, color="slategrey", outline="black", window=win,
                     offset=40)
+    
+    btn_quit = Button(win, Point(180, 140), 30, 10, "QUIT")
+    btn_quit.activate()
+
     # posição da bola
     stickman_hand_height = 48
     stickman_right_disloc = 23
@@ -30,14 +33,28 @@ def cen1():
     scoreboard = ScoreBoard(Point(15, 150), Point(100, 140), win)
     # cria o jogador
     player = StickMan(Point(5, 52), 0, "right", win)
-    # cria uma janela para inputar os valores^
+
+    # cria uma janela para inputar os valores
 
     while win.isOpen():
 
+
+
+        v_window = InputDialog(angle, vel0)
+        option = v_window.interaction()
+
         if option == "Quit":
+            v_window.close()
             break
         elif option == "Fire!":
-            angle, vel0 = 30, 45  # v_window.collect_values()
+            angle, vel0 = v_window.collect_values()
+            v_window.close()
+
+
+            win.master.lift()
+            win.master.focus_force()
+            win.update()
+            win.bind_all("<Key>", win._onKey)
 
             # inicializa a gravação da trajetória
             data_hora_inicio, tempo_atual, trajectory_t, trajectory_x, trajectory_y = start_trajectory()
@@ -46,19 +63,29 @@ def cen1():
                                    ypoint=stickman_hand_height, angle=angle, vel0=vel0, scalar=0)
             player.shoot(win)
             scored = False
+
             while 0 <= tracker.gety() and -10 <= tracker.getx() <= 220:
                 tracker.update_tracker(dt)
+
+                static_colision_detector(tracker, 6, px=0, py=0, normx=0, normy=1)
+                static_colision_detector(tracker, 6, px=205, py=0, normx=-1, normy=0)
+
                 # regista a posição atual da bola
-                tempo_atual = record_point(trajectory_t, trajectory_x, trajectory_y, tempo_atual, tracker.getx(), tracker.gety())
+                tempo_atual = record_point(trajectory_t, trajectory_x, trajectory_y, tempo_atual, tracker.getx(),
+                                           tracker.gety())
+
                 # verifica se marca ponto
                 if not scored and 163 < tracker.getx() < 197 and 95 <= tracker.gety() <= 105:
                     scoreboard.update_score()
-                    # condição para evitar pontos duplicados
                     scored = True
                     scoreboard.message.undraw()
                     scoreboard.unecessary_cheers(win)
+
                 win.update()
                 update(20)
+
+                if tracker.xvel ** 2 + tracker.yvel ** 2 < 0.25:
+                    break
 
             tracker.destroy()
             player.clear_arms()
@@ -77,6 +104,9 @@ def cen1():
 
             if tecla_pressionada in ["G", "g"]:
                 save_trajectory(win, trajectory_t, trajectory_x, trajectory_y, data_hora_inicio, 400, 400)
+
+    if win.isOpen():
+        win.close()
 
 
 def cen3():
@@ -197,4 +227,4 @@ def cen3():
 # cenário 2
 # cen2()
 # cenário 3
-cen3()
+#cen3()
